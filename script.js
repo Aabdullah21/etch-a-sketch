435aec9e4ee57e919297d47c40539c6a53227dcb
Abdullah <abdullah056644@hotmail.com>
2022-08-18 15:23:45 +0300

    Add a function that generates a random color

--- script.js
+++ script.js
@@ -22,3 +22,14 @@ function removeChildren() {
     const boxes = document.querySelectorAll('.small-box');
     boxes.forEach((box) => grid.removeChild(box));
 }
+
+function generateRandomColor() {
+    const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+    let color = '#';
+    for (i = 0; i < 6; i++) {
+        let randomNumber = Math.floor(Math.random() * 16);
+        color += colors[randomNumber];
+    }
+
+    return color;
+}
